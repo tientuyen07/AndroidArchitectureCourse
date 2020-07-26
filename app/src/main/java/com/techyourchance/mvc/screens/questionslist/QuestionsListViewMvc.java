8a8a4f57bee5a3f599d58d73e39691dbb4f3b81a
tientuyen07 <tientuyen07@gmail.com>
2020-07-26 15:16:15 +0700

    Extraction of ViewMvc interface

--- app/src/main/java/com/techyourchance/mvc/screens/questionslist/QuestionsListViewMvc.java
+++ app/src/main/java/com/techyourchance/mvc/screens/questionslist/QuestionsListViewMvc.java
@@ -1,21 +1,18 @@
 package com.techyourchance.mvc.screens.questionslist;
 
-import android.view.View;
-
 import com.techyourchance.mvc.questions.Question;
+import com.techyourchance.mvc.screens.common.ViewMvc;
 
 import java.util.List;
 
-public interface QuestionsListViewMvc {
-    public interface Listener {
+public interface QuestionsListViewMvc extends ViewMvc {
+    interface Listener {
         void onQuestionClicked(Question question);
     }
 
     void registerListener(Listener listener);
 
     void unregisterListener(Listener listener);
-
-    View getRootView();
 
     void bindQuestions(List<Question> questions);
 }
